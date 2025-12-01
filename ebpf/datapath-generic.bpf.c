4f9f8074de124822f2839d40156105147b4446fd
spoonmilk <haku31229@gmail.com>
2025-12-01 01:43:47 -0500

    [prog] Map structure

--- ebpf/datapath-generic.bpf.c
+++ ebpf/datapath-generic.bpf.c
@@ -3,6 +3,73 @@
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_endian.h>
 #include "common.h"
+#include "datapath-generic.h"
+
+// Flow/Ack statistics - calculated per-ack
+struct {
+    __uint(type, BPF_MAP_TYPE_RINGBUF);
+    __uint(max_entries, 256 * 1024);
+} flow_stat_measurements SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_RINGBUF);
+    __uint(max_entries, 256 * 1024);
+} ack_stat_measurements SEC(".maps");
+
+// Flow rate info - continuously updated
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 1000);
+    __type(key, struct flow_key);
+    __type(value, struct flow_rates);
+} flow_rate_map;
+
+// cwnd updates
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 10000);
+    __type(key, struct flow_key);
+    __type(value, struct cwnd_update);
+} cwnd_control SEC(".maps");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct {
     __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
@@ -10,20 +77,10 @@ struct {
     __uint(value_size, sizeof(u32));
 } generic_flow_map SEC(".maps");
 
-struct flow_statistics {
-    struct flow_key flow;
-    u32 packets_in_flight;
-    u32 bytes_in_flight;
-    u32 bytes_pending;
-    u32 rtt_sample_us;
-    u8 was_timeout;
-    u32 rate_incoming;
-    u32 rate_outgoing;
-};
-
 struct {
     __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
     __uint(key_size, sizeof(int));
     __uint(value_size, sizeof(u32));
 } generic_ack_map SEC(".maps");
+
 
